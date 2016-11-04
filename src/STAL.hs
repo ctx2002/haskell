e6f26d066e24e64a30469cc08ec387056384d635
anru chen <ctx2002@gmail.com>
2016-11-04 20:15:48 +1300

    merge

--- src/STAL.hs
+++ src/STAL.hs
@@ -1,4 +1,5 @@
 module STAL
+
 (splitList)
 where
 import           DB
@@ -11,3 +12,16 @@ splitList xs  = split' xs 1 (length xs)
 split' :: [a] -> Int -> Int ->  [([a], [a])]
 split' xs firstNth len |  firstNth < len = (take firstNth xs, drop firstNth xs) : split' xs (firstNth+1) len
                        | otherwise = []
+
+naturals = [0..]
+evens1 = [n | n <- naturals , even n]
+odd1 = [n | n <- naturals , odd n]
+even2 = [ 2*n | n <- naturals]
+small_squares1 = [n^2 | n <- enumFromTo 0 999]
+
+run:: Integer -> [Integer]
+run n
+  | n < 1 = error "argument not positive"
+  | n == 1 = [1]
+  | even n = n : run (div n 2)
+  | odd n = n : run (3*n + 1)
